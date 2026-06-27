813c2397165b2812baa3596384cadcb21dd09ca2
i000o <iosman1096@gmail.com>
2026-06-27 18:32:09 +0100

    blue on current day js

--- js/script.js
+++ js/script.js
@@ -17,14 +17,20 @@ document.addEventListener("DOMContentLoaded", () => {
         const maxAmount = Math.max(...expenses.map(day => day.amount)); 
         const chartContainer = document.getElementById('chart'); 
 
+        const dayNames = ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat']; 
+        const today = dayNames[new Date().getDay()]; 
+
         expenses.forEach(day => { 
             const amount = day.amount; 
             const amountBar = document.createElement('div'); 
             amountBar.classList.add('bar'); 
-            amountBar.style.height = `${(amount / maxAmount) * 100}%`; 
 
+            if (day.day === today) { 
+                amountBar.classList.add('bar-today'); 
+            }
+
+            amountBar.style.height = `${(amount / maxAmount) * 100}%`; 
             chartContainer.appendChild(amountBar); 
         })
     }
-
 }) 
